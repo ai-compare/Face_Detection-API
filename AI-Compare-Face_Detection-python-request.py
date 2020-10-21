eb9c35991744e1b698e36e6aeb190ce1d7860326
ai-compare <67952486+ai-compare@users.noreply.github.com>
2020-10-21 14:23:14 +0200

    Create AI-Compare-Face_Detection-python-request.py

---
+++ AI-Compare-Face_Detection-python-request.py
@@ -0,0 +1,21 @@
+import requests
+
+# You can find the documentation here : https://www.ai-compare.com/v1/redoc/#operation/Face%20Detection
+
+#Enter your API Token
+headers = {  'Authorization': 'Bearer your API Key'} #You can get your free API token here https://www.ai-compare.com/accounts/login/?next=/my_apis
+
+# Select API
+url = 'https://www.ai-compare.com/v1/pretrained/vision/face_detection'
+
+# Select providers, and objects to detect
+payload = {'providers': '[\'google_cloud\', \'cognitives_service\', \'aws\']'}
+
+# Select file to test
+files = [  ('files', open('Picture/example.jpg','rb'))]
+
+# Request to AI COMPARE
+response = requests.request("POST", url, headers=headers, data = payload, files = files)
+
+# Print result
+print(response.text.encode('utf8'))
